1522d573f041b4bb77c0adae6af120e1054084b5
Matti <106517274+sfuniv@users.noreply.github.com>
2022-06-08 15:04:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,32 +41,4 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
 
-#New Section to display fruityvice api response
-#streamlit.header('Fruityvice Fruit Advice!')
-#fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-#streamlit.write('The user entered', fruit_choice)
-
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#take the json version of the response and normalize it
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-#streamlit.dataframe(fruityvice_normalized)
-
-#don't run anything past here while we trouble shoot
-#streamlit.stop()
-
-#import snowflake.connector
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("select * from fruit_load_list")
-#my_data_rows = my_cur.fetchall()
-#streamlit.header("The fruit load list contains:")
-#streamlit.dataframe(my_data_rows)
-
-#Allow the end user to add a fruit to the list
-#add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-#streamlit.write('Thanks for adding', add_my_fruit)
-
 
